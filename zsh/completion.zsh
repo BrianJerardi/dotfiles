e8c6b89c601c4f8082cd7418287f18e9a0aec9b9
Brian Jerardi <bjerardi@gmail.com>
2013-04-16 19:10:25 -0400

    Update zsh to allow completion with an arrow-key driven interface

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,6 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+# autocompletion with an arrow-key driven interface
+zstyle ':completion:*' menu select
